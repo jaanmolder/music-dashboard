3e292e7de10be1230215df5fc0541f0d21bac1ed
jaanmolder <jaan.molder@outlook.com>
2021-12-20 15:53:22 +0200

    yes

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,12 +1,15 @@
 import { GET_ALL_MUSIC_EVENTS } from "./actions";
 
-function getMusicEvents (state = [], action: { type: any; payload: { data: any; }; }) {
-    switch (action.type) {
-        case GET_ALL_MUSIC_EVENTS:
-            return [ action.payload.data, ...state ];
-    }
+function getMusicEvents(
+  state = [],
+  action: { type: any; payload: { data: any } }
+) {
+  switch (action.type) {
+    case GET_ALL_MUSIC_EVENTS:
+      return [action.payload.data, ...state];
+  }
 
-    return state;
+  return state;
 }
 
 export default getMusicEvents;
